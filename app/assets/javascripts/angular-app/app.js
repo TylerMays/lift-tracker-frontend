ee36dba54e2de7540a388bd0738cd882708b2a79
Tyler Mays <richard.tyler.mays@gmail.com>
2016-06-02 19:51:53 -0700

    Add ng Ctrl and ng repeat to test GET from API

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,1 +1,28 @@
-angular.module('LiftTracker', []);
+angular.module('LiftTracker', [])
+.controller('Ctrl', function($scope, $filter, $http, $timeout) {
+
+  console.log("hi there");
+
+  $scope.getJobs = function() {
+    $http.get('https://rails5-bookstore-api.herokuapp.com/books').then(function(response) {
+      console.log(response.data);
+      $scope.list = response.data;
+    });
+  };
+
+  $scope.postJob = function(job) {
+    $http.post('/jobs', job).then(function(response) {
+      $scope.list = response.data;
+      console.log("POST: ",$scope.list);
+    });
+  };
+
+  $scope.putJob = function(job) {
+    $http.put('/jobs', job).then(function(response) {
+      $scope.list = response.data;
+      console.log("PUT: ", $scope.list);
+    });
+  };
+
+  $scope.getJobs();
+});
