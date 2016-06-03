3e44decae7baf13906c8bd1d7e5335115bcb9225
Tyler Mays <richard.tyler.mays@gmail.com>
2016-06-02 20:08:40 -0700

    Prevent rails from mangling angular js files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Prevents Rails in production automatically minifying variables which messes up Angular.
+  config.assets.js_compressor = Uglifier.new(mangle: false)
 end
